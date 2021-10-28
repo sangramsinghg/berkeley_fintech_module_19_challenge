86066c1e4a4484428904eb6cb945d01a96feb045
sangramsinghg <sangramsinghg@yahoo.com>
2021-10-28 00:25:32 -0700

    Calculate wage and write it to the sidebar

--- fintech_finder.py
+++ fintech_finder.py
@@ -146,7 +146,8 @@ st.sidebar.write(account.address)
 # @TODO
 # Call `get_balance` function and pass it your account address
 # Write the returned ether balance to the sidebar
-# YOUR CODE HERE
+ether = get_balance(account.address)
+st.sidebar.write(ether)
 
 ##########################################
 
@@ -237,11 +238,11 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
+wage = hours * hourly_rate
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage)
 
 ##########################################
 # Step 2 - Part 2:
